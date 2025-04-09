e49984d421c2777a26d55ed1e65feab3a55a7c19
sam <161426223+disenosam@users.noreply.github.com>
2025-04-09 10:57:08 -0700

    Update portfolio.tsx

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -39,7 +39,7 @@ const projects: Project[] = [
     category: "branding",
     image: "/melamia.png?height=600&width=800",
     description:
-      "Complete brand identity redesign for a sustainable cosmetics company, including logo, packaging, and brand guidelines.",
+      "Complete brand identity launch from stcrach for  a supplements cosmetics company, including logo, packaging, and brand guidelines applying all Mexican regulations for dietary supplements.",
     client: "Melamia Beauty Co.",
     year: "2022",
     link: "#",
