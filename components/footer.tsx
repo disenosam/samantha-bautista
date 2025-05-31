fa2caf71518ca17494a8d4ac6e9bdec5c91dd9de
sam <161426223+disenosam@users.noreply.github.com>
2025-05-31 12:38:08 -0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
           </div>
 
           <div className="flex space-x-4">
-            <Link href="#" className="text-muted-foreground hover:text-primary transition-colors">
+            <Link href="https://www.instagram.com/sxmtero/" className="text-muted-foreground hover:text-primary transition-colors">
               <Instagram className="h-5 w-5" />
               <span className="sr-only">Instagram</span>
             </Link>
